77c86432c20f545dd1c7f6512903d71f114a3503
safinxr <safinxr@gmail.com>
2024-05-18 17:21:54 +0600

    xr

--- dist/app/app.js
+++ dist/app/app.js
@@ -6,11 +6,36 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get("/", (req, res) => {
-    res.send("Hey yo");
+// parsers
+app.use(express_1.default.json());
+app.use(express_1.default.text());
+const userRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+userRouter.get("/create-users", (req, res) => {
+    const data = req.body;
+    res.json({
+        message: "everything okk",
+        data: data
+    });
+});
+// middleWare
+const loger = (req, res, next) => {
+    console.log("loger");
+    console.log(req.url);
+    next();
+};
+app.get("/", loger, (req, res) => {
+    // res.send(`home route user id: ${req.params.userId}`);
+    res.send(`home `);
+});
+app.get("/gg", (req, res) => {
+    console.log(req.query);
+    res.send("its from gg");
 });
 app.post("/", (req, res) => {
     console.log(req.body);
-    res.send("post working");
+    res.json({
+        message: "Post data ",
+    });
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,48 @@
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 const port = 3000;
 
+// parsers
+app.use(express.json());
+app.use(express.text());
 
+const userRouter = express.Router();
 
-app.get("/", (req:Request, res:Response) => {
-  res.send("Hey yo");
+app.use("/api/v1/users", userRouter);
+
+userRouter.get(
+  "/create-users",
+  (req: Request, res: Response) => {
+    const data = req.body
+    res.json({
+      message: "yooooo",
+      data: data
+    })
+  }
+);
+
+// middleWare
+const loger = (req: Request, res: Response, next: NextFunction) => {
+  console.log("loger");
+  console.log(req.url);
+  next();
+};
+
+app.get("/", loger, (req: Request, res: Response) => {
+  // res.send(`home route user id: ${req.params.userId}`);
+  res.send(`home `);
 });
 
-app.post("/", (req:Request, res:Response) =>{
-  console.log(req.body);
-  res.send("post working")
-})
+app.get("/gg", (req: Request, res: Response) => {
+  console.log(req.query);
+  res.send("its from gg");
+});
 
-export default app
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({
+    message: "Post data ",
+  });
+});
+
+export default app;
